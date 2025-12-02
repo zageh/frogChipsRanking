450493919d5f9d7a0a48a6ecff7107323fcd53a7
zageh <zageh120@outlook.com>
2025-12-02 15:38:41 +0800

    大清洗后重建

--- .next/dev/static/chunks/src_app_page_56f87536.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_c8a257d0._.js",
-  "static/chunks/src_7366ba15._.js"
-],
-    source: "dynamic"
-});
