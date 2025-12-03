941c73a90c8d4693e5a5853e822a0becdf9307c2
zageh <zageh120@outlook.com>
2025-12-04 00:44:50 +0800

    no1

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import supabase from '../../../utils/supabase'
+import supabase from '../../utils/supabase'
 
 export default function LoginPage() {
   const [username, setUsername] = useState('')
