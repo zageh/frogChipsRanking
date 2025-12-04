52a92020f8119e50ac172d194d642a9758f77a3d
zageh <zageh120@outlook.com>
2025-12-04 12:16:08 +0800

    1,0ver

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://biqntzsdzfndxtvhgpux.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default supabase
